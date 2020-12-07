524d3738012ef42dd249a93ffb26c80a9150667a
danielson <danielson378@gmail.com>
2020-12-08 00:33:10 +0900

    src

---
+++ src/com/action/AdminAction.java
@@ -0,0 +1,105 @@
+package com.action;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.entity.Admin;
+import com.service.AdminService;
+
+@Namespace("/admin")
+@Results({
+	@Result(name="login", location="/admin/login.jsp"),
+	@Result(name="index", location="/admin/index.jsp"),
+	@Result(name="adminEdit", location="/admin/admin_edit.jsp"),
+	@Result(name="readmin", type="redirect", location="index.action"),
+})	
+@SuppressWarnings("serial")
+public class AdminAction extends BaseAction{
+	
+	private Admin admin;
+	
+	private String msg;
+	
+	@Autowired
+	private AdminService adminService;
+	
+	
+	
+	/**
+	 * 登录
+	 * @return
+	 */
+	@Action("login")
+	public String login(){
+		Admin loginAdmin = adminService.get(admin.getUsername(), admin.getPassword());
+		if (loginAdmin==null) {
+			msg = "用户名或密码错误";
+			return "login";
+		}
+		// 将登录用户信息存入session
+		getSession().setAttribute("admin", loginAdmin);
+		return "readmin"; // 重定向到管理首页
+	}
+
+	/**
+	 * 后台首页
+	 * @return
+	 */
+	@Action("index")
+	public String index(){
+		msg = "恭喜你! 登录成功了";
+		return "index";
+	}
+	
+	/**
+	 * 管理员编辑
+	 * @return
+	 */
+	@Action("adminEdit")
+	public String adminEdit(){
+		return "adminEdit";
+	}
+	
+	/**
+	 * 管理员更新
+	 * @return
+	 */
+	@Action("adminUpdate")
+	public String adminUpdate(){
+		if (admin.getPassword()==null || admin.getPassword().trim().isEmpty()) {
+			msg = "原密码不能为空";
+			return "adminEdit";
+		}
+		if (!adminService.check(admin.getId(), admin.getPassword())) {
+			msg = "原密码错误";
+			return "adminEdit";
+		}
+		if(adminService.update(admin)){
+			getSession().setAttribute("admin", adminService.get(admin.getId())); // 刷新session
+			msg = "修改成功";
+			return "adminEdit";
+		}
+		return null;
+	}
+
+
+	public Admin getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+}
